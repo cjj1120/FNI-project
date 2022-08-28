a30add3109e20ec3992d5b4e0d52cfaf8d9a4970
Your Name <cjj8168@gmail.com>
2022-08-28 23:02:08 +0800

    add table view

--- src/fni/fni_page/urls.py
+++ src/fni/fni_page/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('inflation', views.inflation),  
-    path('about', views.about)  
+    path('', views.home, name="home"),
+    path('inflation', views.inflation, name = "inflation"),  
+    path('about', views.about, name = "about")  
 ]
--- src/fni/fni_page/views.py
+++ src/fni/fni_page/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import * 
 
 
 def home(request):
 	return render(request, 'fni_page/home.html')
 
 def inflation(request):
-	return render(request, 'fni_page/inflation.html')
+    trends = Trend.objects.order_by('-data_created')[:5]
+    return render(request, 'fni_page/inflation.html', {'trend_list': trends})
 
 def about(request):
 	return render(request, 'fni_page/about.html')
